0d15a4bff93e29e6ebce3f3f5a5f227687eef562
Toshirro <30690342+Toshhirro@users.noreply.github.com>
2020-12-14 12:47:25 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,10 +1,10 @@
 
 
-if(!(Test-Path -Path "C:\Users\Abby\Pictures\Camera Roll\sample.png")){      # copy pasting a file to a folder if it doesn't exist
+if(!(Test-Path -Path "\\to\location")){      # copy pasting a file to a folder if it doesn't exist
 copy-item -path "\\from\location" -destination "\\to\location"}
 
 
-(get-Item -path "c:\sample\sample.txt").Encrypt()                #encrypting a sample text file
+(get-Item -path "c:\sample.txt").Encrypt()                #encrypting a sample text file
 
 
 Start-Sleep -Seconds 5
